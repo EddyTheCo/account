5616769742d08c1310868286e07dbae02ca15741
EddyGlezz <eddyglezz95@gmail.com>
2022-12-16 21:44:55 +0100

    init repo

---
+++ account.cpp
@@ -0,0 +1,65 @@
+
+#include <QRandomGenerator>
+#include"encoding/qbech32.hpp"
+#include"crypto/qed25519.hpp"
+#include"account.hpp"
+#include <QCryptographicHash>
+
+address_bundle::address_bundle(const std::pair<QByteArray,QByteArray>& key_pair_m,QString hrp_m):key_pair(key_pair_m),
+	hrp(hrp_m), addr_hash(get_hash()), addr(get_address())
+{ };
+
+
+qblocks::c_array address_bundle::get_hash(void)const
+{
+	const auto hash=qblocks::c_array(QCryptographicHash::hash(key_pair.first,QCryptographicHash::Blake2b_256));
+	return hash;
+}
+
+QString address_bundle::get_address(void)const
+{
+	auto pub=addr_hash;
+	pub.push_front('\x00');
+	const auto addr=qencoding::qbech32::Iota::encode(hrp,pub);
+	return addr;
+}
+
+QByteArray account::get_random_seed(void)const{
+	QByteArray var;
+	auto buffer=QDataStream(&var,QIODevice::WriteOnly | QIODevice::Append);
+	for(auto i=0;i<8;i++)
+	{
+		quint32 value = QRandomGenerator::global()->generate();
+		buffer<<value;
+	}
+	return QByteArray::fromHex("20b5f5a1dc76bc385d27fe9de15d97c0f338ff1389ecae3f7c9bd748707e141e");
+	//return var;
+}
+account::account():master_(get_random_seed()),publish_path({44,4219,0,0,0}),last_index(0),last_mov_index(0)
+{
+}
+address_bundle account::get_payment_addr(void) const
+{
+	QVector<quint32> path={44,4219,1,0};
+	path.push_back(last_index);
+	auto keys=master_.slip10_key_from_path(path);
+	return address_bundle(qed25519::create_keypair(keys.secret_key()));
+}
+address_bundle account::get_consolidation_addr(void) const
+{
+	QVector<quint32> path={44,4219,2,0};
+	path.push_back(last_mov_index);
+	auto keys=master_.slip10_key_from_path(path);
+	return address_bundle(qed25519::create_keypair(keys.secret_key()));
+}
+address_bundle account::get_publish_addr(void) const
+{
+	auto keys=master_.slip10_key_from_path(publish_path);
+	const auto edkeys=qed25519::create_keypair(keys.secret_key());
+	return address_bundle(edkeys);
+}
+
+qblocks::signature address_bundle::sign(const QByteArray & message)const
+{
+	return qblocks::signature(qed25519::sign(key_pair,message));
+}
---
+++ include/account.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include"crypto/qslip10.hpp"
+#include"block/qblock.hpp"
+using namespace qcrypto;
+using namespace qiota;
+class address_bundle
+{
+	public:
+		address_bundle(const std::pair<QByteArray,QByteArray>& key_pair_m,QString hrp_m="rms");
+		const std::pair<QByteArray,QByteArray> key_pair;
+		const QString hrp;
+		const qblocks::c_array addr_hash;
+		const QString addr;
+	
+		qblocks::signature sign(const QByteArray & message)const;
+
+	private:
+
+		qblocks::c_array get_hash(void)const;
+
+		QString get_address(void)const;
+};
+class account
+{
+
+	private:
+		QByteArray get_random_seed(void)const;
+	public:
+		account();
+		address_bundle get_payment_addr(void) const;
+		address_bundle get_consolidation_addr(void) const;
+		address_bundle get_publish_addr(void) const;
+		void next_payment_addr(void){last_index++;}
+		void next_consolidation_addr(void){last_mov_index++;}
+
+	private:
+		const Master_key master_;
+		quint32 last_index;
+		quint32 last_mov_index;
+		const QVector<quint32> publish_path;
+};
+
