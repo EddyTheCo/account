7c137d520fc9e8308e6f71e25900fc714b043d6e
Eduardo Gonzalez Lazo <30321688+EddyTheCo@users.noreply.github.com>
2023-06-08 10:15:11 +0200

    Develop (#14)
    * Fixes module imports (#6)
    
    * use the cmake qmlimportpath just in case it is used by qtcreator
    
    * added ignore for docs
    
    * fix the build of static lib
    
    * added the option to build using mthreads
    
    * make available the modules for documentation building
    
    * the module linkage is the same of qt (#11)
    
    * added release action here (#12)
    
    
    * use qaddressbundle that works for windows shared
    
    * trigger on default types of pull request
    
    * added README

---
+++ README.md
@@ -0,0 +1,13 @@
+# Account QML 
+
+Implements a custom QML type defined in C++. The custom type takes care of producing deterministic addresses from a seed.
+
+
+## API reference
+
+You can read the [API reference](https://eddytheco.github.io/account/), or generate it yourself like
+```
+cmake -DBUILD_DOCS=ON ../
+cmake --build . --target doxygen_docs
+```
+
--- include/account.hpp
+++ include/account.hpp
@@ -13,46 +13,46 @@ using namespace qcrypto;
 
 class Account : public QObject
 {
-    Q_OBJECT
+	Q_OBJECT
 
-    Q_PROPERTY(QString  seed READ seed WRITE set_seed NOTIFY seedChanged)
+		Q_PROPERTY(QString  seed READ seed WRITE set_seed NOTIFY seedChanged)
 
-    QML_ELEMENT
-    QML_SINGLETON
+		QML_ELEMENT
+		QML_SINGLETON
 
-public:
-    Account(QObject *parent = nullptr);
-    QString seed(void)const{return QString(seed_.toHex());}
-    void set_seed(QString seedstr);
-    static address_bundle get_addr(const QVector<quint32>& subpath); //(0,0,0)
-    Q_INVOKABLE static QString addr(const QVector<quint32> subpath)
-    {return get_addr(subpath).get_address()->addr().toHexString();}
-    Q_INVOKABLE static QString addr_bech32(const QVector<quint32> subpath,QString hrp)
-    {return get_addr(subpath).get_address_bech32(hrp);};
-    Q_INVOKABLE static void set_path(const QVector<quint32>& path_m){path_=path_m;}
-    Q_INVOKABLE static QJsonObject path_json(const QVector<quint32> subpath)
-    {
-        return get_addr(subpath).get_address()->get_Json();
-    };
-    Q_INVOKABLE static QJsonObject bech32_json(const QString &bech32addr)
-    {
-        auto addr=qencoding::qbech32::Iota::decode(bech32addr);
-        if(addr.second.size())
-        return Address::from_array(addr.second)->get_Json();
-        return QJsonObject();
-    };
-    Q_INVOKABLE static QString json_bech32(const QJsonValue &json_addr,const QString& hrp)
-    {
-        const auto addr=Address::from_(json_addr)->addr();
-        return qencoding::qbech32::Iota::encode(hrp,addr);
-    };
+	public:
+		Account(QObject *parent = nullptr);
+		QString seed(void)const{return QString(seed_.toHex());}
+		void set_seed(QString seedstr);
+		static address_bundle get_addr(const QVector<quint32>& subpath); //(0,0,0)
+		Q_INVOKABLE static QString addr(const QVector<quint32> subpath)
+		{return get_addr(subpath).get_address()->addr().toHexString();}
+		Q_INVOKABLE static QString addr_bech32(const QVector<quint32> subpath,QString hrp)
+		{return get_addr(subpath).get_address_bech32(hrp);};
+		Q_INVOKABLE static void set_path(const QVector<quint32>& path_m){path_=path_m;}
+		Q_INVOKABLE static QJsonObject path_json(const QVector<quint32> subpath)
+		{
+			return get_addr(subpath).get_address()->get_Json();
+		};
+		Q_INVOKABLE static QJsonObject bech32_json(const QString &bech32addr)
+		{
+			auto addr=qencoding::qbech32::Iota::decode(bech32addr);
+			if(addr.second.size())
+				return Address::from_array(addr.second)->get_Json();
+			return QJsonObject();
+		};
+		Q_INVOKABLE static QString json_bech32(const QJsonValue &json_addr,const QString& hrp)
+		{
+			const auto addr=Address::from_(json_addr)->addr();
+			return qencoding::qbech32::Iota::encode(hrp,addr);
+		};
 signals:
-    void seedChanged();
-private:
-    void set_random_seed(void);
-    static Master_key master_;
-    static QByteArray seed_;
-    static QVector<quint32> path_;
+		void seedChanged();
+	private:
+		void set_random_seed(void);
+		static Master_key master_;
+		static QByteArray seed_;
+		static QVector<quint32> path_;
 
 };
 
