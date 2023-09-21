4cc7cb34e94e58eb5ce2ce6ed6accef91e550d0b
Eduardo Gonzalez Lazo <30321688+EddyTheCo@users.noreply.github.com>
2023-09-21 10:38:35 +0200

    set the local conf on the build directory (#28) (#29)

--- include/account.hpp
+++ include/account.hpp
@@ -28,26 +28,27 @@ class ACCOU_EXPORT Account : public QObject
 
 	public:
 		Account(QObject *parent = nullptr);
+        static Account* instance(){return m_instance;};
 		QString seed(void)const{return QString(seed_.toHex());}
 		void set_seed(QString seedstr);
-		static address_bundle get_addr(const QVector<quint32>& subpath); //(0,0,0)
-		Q_INVOKABLE static QString addr(const QVector<quint32> subpath)
+        address_bundle get_addr(const QVector<quint32>& subpath); //(0,0,0)
+        Q_INVOKABLE QString addr(const QVector<quint32> subpath)
 		{return get_addr(subpath).get_address()->addr().toHexString();}
-		Q_INVOKABLE static QString addr_bech32(const QVector<quint32> subpath,QString hrp)
+        Q_INVOKABLE QString addr_bech32(const QVector<quint32> subpath,QString hrp)
 		{return get_addr(subpath).get_address_bech32(hrp);};
-		Q_INVOKABLE static void set_path(const QVector<quint32>& path_m){path_=path_m;}
-		Q_INVOKABLE static QJsonObject path_json(const QVector<quint32> subpath)
+        Q_INVOKABLE void set_path(const QVector<quint32>& path_m){path_=path_m;}
+        Q_INVOKABLE QJsonObject path_json(const QVector<quint32> subpath)
 		{
 			return get_addr(subpath).get_address()->get_Json();
 		};
-		Q_INVOKABLE static QJsonObject bech32_json(const QString &bech32addr)
+        Q_INVOKABLE QJsonObject bech32_json(const QString &bech32addr)
 		{
 			auto addr=qencoding::qbech32::Iota::decode(bech32addr);
 			if(addr.second.size())
 				return Address::from_array(addr.second)->get_Json();
 			return QJsonObject();
 		};
-		Q_INVOKABLE static QString json_bech32(const QJsonValue &json_addr,const QString& hrp)
+        Q_INVOKABLE QString json_bech32(const QJsonValue &json_addr,const QString& hrp)
 		{
 			const auto addr=Address::from_(json_addr)->addr();
 			return qencoding::qbech32::Iota::encode(hrp,addr);
@@ -56,9 +57,10 @@ signals:
 		void seedChanged();
 	private:
 		void set_random_seed(void);
-		static Master_key master_;
-		static QByteArray seed_;
-		static QVector<quint32> path_;
+        Master_key master_;
+        QByteArray seed_;
+        QVector<quint32> path_;
+        static Account * m_instance;
 
 };
 
