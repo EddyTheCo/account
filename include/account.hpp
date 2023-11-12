1529380240898879a89a5961f008402d4f1e43fb
Eduardo Gonzalez Lazo <30321688+EddyTheCo@users.noreply.github.com>
2023-11-12 11:55:05 +0100

    added and option to use qml, set the cronstructor private and improve the singleton programing

--- include/account.hpp
+++ include/account.hpp
@@ -1,11 +1,15 @@
 #pragma once
-#include"qaddr_bundle.hpp"
 #include"crypto/qslip10.hpp"
+#include"block/qaddresses.hpp"
+#include"encoding/qbech32.hpp"
 #include<QObject>
 #include<QJsonObject>
 #include<QString>
 #include<QByteArray>
+
+#if defined(USE_QML)
 #include <QtQml/qqmlregistration.h>
+#endif
 
 #include <QtCore/QtGlobal>
 #if defined(WINDOWS_ACCOU)
@@ -13,54 +17,72 @@
 #else
 #define ACCOU_EXPORT Q_DECL_IMPORT
 #endif
+
 using namespace qiota;
 using namespace qiota::qblocks;
 using namespace qcrypto;
 
 class ACCOU_EXPORT Account : public QObject
 {
-	Q_OBJECT
 
-		Q_PROPERTY(QString  seed READ seed WRITE set_seed NOTIFY seedChanged)
+    Q_OBJECT
+#if defined(USE_QML)
+    Q_PROPERTY(QString  seed READ seed WRITE setSeed NOTIFY seedChanged)
+    Q_PROPERTY(Account  instance READ instance NOTIFY instanceChanged)
+    QML_ELEMENT
+    QML_SINGLETON
+#endif
+    static QByteArray setRandomSeed(void);
+    Account(QObject *parent = nullptr,QByteArray seed=setRandomSeed());
+public:
+    static Account* instance();
+    QString seed(void)const{return QString(seed_.toHex());}
+    void setSeed(QString seedstr);
+    std::pair<QByteArray,QByteArray> getKeys(const QVector<quint32>& subpath); //(0,0,0)
+    std::shared_ptr<Address> getAddr(const QVector<quint32> subpath)
+    {
+        return std::shared_ptr<Address>
+            (new Ed25519_Address(QCryptographicHash::hash(getKeys(subpath).first,QCryptographicHash::Blake2b_256)));
+    }
+    c_array getAddrArray(const QVector<quint32> subpath)
+    {
+        return getAddr(subpath)->addr();
+    }
+    Q_INVOKABLE QString getAddrHex(const QVector<quint32> subpath)
+    {
+        return getAddrArray(subpath).toHexString();
+    }
 
-		QML_ELEMENT
-		QML_SINGLETON
-
-	public:
-		Account(QObject *parent = nullptr);
-        static Account* instance(){return m_instance;};
-		QString seed(void)const{return QString(seed_.toHex());}
-		void set_seed(QString seedstr);
-        address_bundle get_addr(const QVector<quint32>& subpath); //(0,0,0)
-        Q_INVOKABLE QString addr(const QVector<quint32> subpath)
-		{return get_addr(subpath).get_address()->addr().toHexString();}
-        Q_INVOKABLE QString addr_bech32(const QVector<quint32> subpath,QString hrp)
-		{return get_addr(subpath).get_address_bech32(hrp);};
-        Q_INVOKABLE void set_path(const QVector<quint32>& path_m){path_=path_m;}
-        Q_INVOKABLE QJsonObject path_json(const QVector<quint32> subpath)
-		{
-			return get_addr(subpath).get_address()->get_Json();
-		};
-        Q_INVOKABLE QJsonObject bech32_json(const QString &bech32addr)
-		{
-			auto addr=qencoding::qbech32::Iota::decode(bech32addr);
-			if(addr.second.size())
-				return Address::from_array(addr.second)->get_Json();
-			return QJsonObject();
-		};
-        Q_INVOKABLE QString json_bech32(const QJsonValue &json_addr,const QString& hrp)
-		{
-			const auto addr=Address::from_(json_addr)->addr();
-			return qencoding::qbech32::Iota::encode(hrp,addr);
-		};
+    Q_INVOKABLE QString getAddrBech32(const QVector<quint32> subpath,QString hrp)
+    {
+        return qencoding::qbech32::Iota::encode(hrp,getAddrArray(subpath));
+    };
+    Q_INVOKABLE void set_path(const QVector<quint32>& path_m){path_=path_m;}
+    Q_INVOKABLE QJsonObject path_json(const QVector<quint32> subpath)
+    {
+        return getAddr(subpath)->get_Json();
+    };
+    Q_INVOKABLE QJsonObject bech32Json(const QString &bech32addr)
+    {
+        auto addr=qencoding::qbech32::Iota::decode(bech32addr);
+        if(addr.second.size())
+            return Address::from_array(addr.second)->get_Json();
+        return QJsonObject();
+    };
+    Q_INVOKABLE QString jsonBech32(const QJsonValue &json_addr,const QString& hrp)
+    {
+        const auto addr=Address::from_(json_addr)->addr();
+        return qencoding::qbech32::Iota::encode(hrp,addr);
+    };
 signals:
-		void seedChanged();
-	private:
-		void set_random_seed(void);
-        Master_key master_;
-        QByteArray seed_;
-        QVector<quint32> path_;
-        static Account * m_instance;
+    void seedChanged();
+    void instanceChanged();
+private:
+
+    Master_key master_;
+    QByteArray seed_;
+    QVector<quint32> path_;
+    static Account * m_instance;
 
 };
 
--- src/account.cpp
+++ src/account.cpp
@@ -6,37 +6,39 @@
 
 
 Account* Account::m_instance=nullptr;
-void Account::set_random_seed(void){
-    auto buffer=QDataStream(&seed_,QIODevice::WriteOnly | QIODevice::Append);
-
-	for(auto i=0;i<8;i++)
-	{
-		quint32 value = QRandomGenerator::global()->generate();
-		buffer<<value;
-    }
-
-}
-Account::Account(QObject *parent):QObject(parent),master_(QByteArray(32,0)),path_({44,4219})
+Account* Account::instance()
 {
-    if(!m_instance)m_instance=this;
-    set_random_seed();
-    master_=Master_key(seed_);
+    if (!m_instance) m_instance=new Account();
+    return m_instance;
 }
-address_bundle Account::get_addr(const QVector<quint32>& subpath)
+QByteArray Account::setRandomSeed(void){
+    auto seed=QByteArray(32,0);
+    auto buffer=QDataStream(&seed,QIODevice::WriteOnly | QIODevice::Append);
+    for(auto i=0;i<8;i++)
+    {
+        quint32 value = QRandomGenerator::global()->generate();
+        buffer<<value;
+    }
+    return seed;
+}
+void Account::setSeed(QString seedstr)
+{
+    auto var=QByteArray::fromHex(seedstr.toUtf8());
+    if(var.size()>=32&&seedstr!=Account::instance()->seed())
+    {
+        emit seedChanged();
+        m_instance=new Account(Account::instance()->parent(),var);
+        deleteLater();
+    }
+}
+Account::Account(QObject *parent,QByteArray seed):QObject(parent),master_(seed),path_({44,4219})
+{
+}
+std::pair<QByteArray,QByteArray> Account::getKeys(const QVector<quint32>& subpath)
 {
     QVector<quint32> fullpath=path_;
     fullpath.append(subpath);
     auto keys=master_.slip10_key_from_path(fullpath);
-	return address_bundle(qed25519::create_keypair(keys.secret_key()));
+    return qed25519::create_keypair(keys.secret_key());
 }
-void Account::set_seed(QString seedstr)
-{
 
-    auto var=QByteArray::fromHex(seedstr.toUtf8());
-    if(var.size()>=32&&var!=seed_)
-    {
-        seed_=var;
-        master_=Master_key(seed_);
-        emit seedChanged();
-    }
-}
